32ed64b983af1be210e22793cd4804dd4c4d4c45
Madankumar <143166440+ksmadankumar@users.noreply.github.com>
2023-12-06 13:34:43 +0530

    Update main.py
    Using link comprehension in guessed states code and commented out the old code

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ guessed_state = []
 while len(guessed_state) < 50:
     answer_state = screen.textinput(title=f" {len(guessed_state)} / 50", prompt="What state comes next?").title()
     if answer_state == "Exit":
-        missing_state = []
-        for state in all_states:
-            if state not in guessed_state:
-                missing_state.append(state)
+        #missing_state = [state for state in all_states if states not in gurssed_state]
+        #for state in all_states:
+            #if state not in guessed_state:
+                #missing_state.append(state)
         new_data = pandas.DataFrame(missing_state)
         new_data.to_csv("states_to_learn.csv")
         break
